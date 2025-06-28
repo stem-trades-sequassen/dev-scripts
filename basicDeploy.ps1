04ab33523f9d86fdc2fb787de75a025a90f76218
Tyler Heinz <144669496+tylerjheinz@users.noreply.github.com>
2025-06-27 22:01:02 -0400

    Update basicDeploy.ps1

--- basicDeploy.ps1
+++ basicDeploy.ps1
@@ -1,1 +1,21 @@
-Hello World
+# Hostname Update
+Write-Host "The hostname of this computer is: $env:computername"
+$renameInput = Read-Host -Prompt "Do you need to update this hostname? [True/False]"
+try 
+{
+    $renameInput = [System.Convert]::ToBoolean($renameInput)
+}
+catch 
+{
+    Write-Host "Invalid Input."
+    $renameInput = Read-Host -Prompt "Do you need to update this hostname? [True/False]"
+    $renameInput = [System.Convert]::ToBoolean($renameInput)
+}
+if ($renameInput -eq $true)
+{
+    Write-Host "Renaming Host"
+    $hostNewName = Read-Host -Prompt "New Hostname"
+    Rename-Computer -NewName $hostNewName
+} else {
+    Write-Host "No changes made."
+}
